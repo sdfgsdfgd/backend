3632fac4e11c562b37ad5cf6c7f2b405e055b4a8
sdfgsdfg <kaanixir@gmail.com>
2025-10-07 21:23:41 +1100

    revert to nonsudo spin up

--- server/src/main/kotlin/Webhooks.kt
+++ server/src/main/kotlin/Webhooks.kt
@@ -45,7 +45,7 @@ private val deploymentProfiles: Map<String, DeploymentProfile> = mapOf(
     "server-py" to DeploymentProfile(
         repoFullName = "sdfgsdfgd/server_py",
         commands = listOf(
-            "sudo -u x XDG_RUNTIME_DIR=/run/user/1000 systemctl --user restart server_py.service",
+            "XDG_RUNTIME_DIR=/run/user/1000 systemctl --user restart server_py.service",
         ),
     ),
 )
