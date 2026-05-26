05f6d24720a994e8c7711b2106588060113d8a2e
sdfgsdfg <kaanixir@gmail.com>
2026-05-27 03:40:27 +1000

    Focus dashboard canvas on boot

--- dashboard/web/src/wasmJsMain/kotlin/net/sdfgsdfg/dashboard/web/Main.kt
+++ dashboard/web/src/wasmJsMain/kotlin/net/sdfgsdfg/dashboard/web/Main.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 import kotlinx.browser.window
 import net.sdfgsdfg.dashboard.DashboardApp
+import org.w3c.dom.HTMLElement
 import kotlin.js.ExperimentalWasmJsInterop
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalWasmJsInterop::class)
@@ -14,6 +15,15 @@ fun main() {
     }
     window.setTimeout({
         document.getElementById("boot")?.classList?.add("boot-done")
+        focusDashboard()
+        window.setTimeout({ focusDashboard(); null }, 150)
         null
     }, 900)
+}
+
+private fun focusDashboard() {
+    val host = document.getElementById("dashboard") as? HTMLElement ?: return
+    val target = host.firstElementChild as? HTMLElement ?: host
+    target.tabIndex = -1
+    target.focus()
 }
